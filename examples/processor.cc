dfd26718ebe27a40bb2e5c6e4f4aa5b5e67e512d
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-09-01 23:27:11 +0100

    processor example

---
+++ examples/processor.cc
@@ -0,0 +1,42 @@
+#include <channel.hh>
+#include <processor.hh>
+#include <iostream>
+
+namespace {
+
+using namespace std;
+using namespace kthread;
+
+struct consumer
+{
+  void operator()(int i) const
+  {
+    cout << i << '\n';
+  }
+};
+
+void fibonacci(int n, sender<int> tx)
+{
+  int a = 0;
+  int b = 1;
+
+  for(int i = 0; i < n; ++i)
+  {
+    tx.push(a);
+
+    b = b + a;
+    a = b - a;
+  }
+}
+
+}
+
+int main()
+{
+  processor<int, consumer> p;
+  p.connect();
+
+  fibonacci(10, p.tx());
+
+  return 0;
+}
